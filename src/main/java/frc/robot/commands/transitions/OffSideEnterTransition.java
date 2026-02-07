d35279814c91411a890d0df12ae7e9fc136ac3fd
Jacob M <jcmertz@outlook.com>
2026-02-07 11:34:43 -0500

    added HubState class

--- src/main/java/frc/robot/commands/transitions/OffSideEnterTransition.java
+++ src/main/java/frc/robot/commands/transitions/OffSideEnterTransition.java
@@ -1,6 +1,19 @@
 package frc.robot.commands.transitions;
 
+import ControlAnnotations.Transition;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.SetMode;
+import frc.robot.commands.basicCommands.shooterStack.DisableShooting;
+import frc.robot.utils.RobotStates;
 
+@Transition
 public class OffSideEnterTransition extends SequentialCommandGroup {
+
+    public OffSideEnterTransition() {
+        addCommands(
+                new DisableShooting(),
+                new SetMode(RobotStates.offSideState)
+        );
+    }
+
 }
