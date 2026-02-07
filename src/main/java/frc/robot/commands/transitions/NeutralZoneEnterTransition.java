f671361fe66eafa6ec1e36f3d188f09561ba7a35
Alex_77983 <alexronk64@gmail.com>
2026-02-07 11:34:51 -0500

    Added the HubInactiveTransition code

--- src/main/java/frc/robot/commands/transitions/NeutralZoneEnterTransition.java
+++ src/main/java/frc/robot/commands/transitions/NeutralZoneEnterTransition.java
@@ -3,4 +3,7 @@ package frc.robot.commands.transitions;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class NeutralZoneEnterTransition extends SequentialCommandGroup {
+
+
+
 }
